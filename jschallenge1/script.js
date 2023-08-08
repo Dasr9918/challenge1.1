e3e363f3bc12f62e16449c4c2dea65d075556341
Dasr9918 <140861092+Dasr9918@users.noreply.github.com>
2023-08-08 09:52:15 -0500

    Add files via upload

---
+++ jschallenge1/script.js
@@ -0,0 +1,24 @@
+/* 1- Creacion de la funcion regular */
+
+//function regularFunction(numeros){
+  //return numeros.test(numeros);
+//}
+
+//Creo el arreglo en donde va a estar el numero a verificar si es par o impar
+let numero = [4];
+
+//Uso del .every() para verificar si todos los elementos de un arreglo cumplen con una condición determinada.
+//sonParesoImpar = numero.every(numeros => numeros % 2 == 0);
+
+//Imprimo en consola.
+//console.log(sonParesoImpar);
+
+/* 2- Creacion de la funcion de flecha */
+
+const arrowFunction = () => sonParesoImpar = numero.every(numeros => numeros % 2 == 0)
+console.log(sonParesoImpar)
+
+
+
+
+
